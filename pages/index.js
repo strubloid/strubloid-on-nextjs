2eaaa9405345875818ecbf71aef4b87661c6107c
strubloid <strubloid@gmail.com>
2020-09-16 13:33:42 +0100

    creating basic structure for css files

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+
+
+    </div>
+  )
+}
