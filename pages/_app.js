6dfbebf9c5cbce5b16d01d3d8acf51ec9bd32cef
strubloid <strubloid@gmail.com>
2020-09-18 16:51:30 +0100

    upgrade of the homepage layout with new image from brasil

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "@scss-global/_buttons.scss";
 import "@scss-global/_general_mixins.scss";
 import "@css/strubloid.css";
 import "@css/components.css";
+import "public/scss/global/_fonts.scss";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
