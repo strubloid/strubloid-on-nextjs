4f47d32fbb55cef62f3247ac77a3763163b5c5a3
strubloid <strubloid@gmail.com>
2020-09-21 22:21:24 +0100

    adding a log to see what is getting in the request object

--- components/shared/BaseUrl.jsx
+++ components/shared/BaseUrl.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 export function getBaseUrl(req) {
     let protocol = 'https:'
+
     let host = req ? req.headers.host : window.location.hostname
+    console.log(req);
     if (host.indexOf('localhost') > -1) {
         host = 'localhost:3333'
         protocol = 'http:'
