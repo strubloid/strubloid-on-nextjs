8ce05775b04f5411e2ec085dc7f8f55944f5796c
strubloid <joserafaelmb@gmail.com>
2020-09-22 17:05:30 +0100

    update stage

---
+++ components/netifly-environment.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const keys = [
+    `GOOGLE_KEY=${process.env.GOOGLE_KEY}\n`,
+    `MONGO_URI=${process.env.MONGO_URI}\n`,
+    `SERVER_URL=${process.env.SERVER_URL}\n`,
+]
+
+const lineToWrite = keys.join('');
+console.log(lineToWrite)
+
+fs.writeFileSync('./.env-test', `${lineToWrite}`)
+
+
