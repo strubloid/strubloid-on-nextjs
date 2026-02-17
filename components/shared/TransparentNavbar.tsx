b70688b48078db83b3dd58ba3b589699cf8f2067
Strubloid <strubloid@gmail.com>
2026-02-17 20:23:39 +0000

    fixing base

--- components/shared/TransparentNavbar.tsx
+++ components/shared/TransparentNavbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Link from "next/link";
-import { Button, Collapse, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown, NavbarBrand, Navbar, NavItem, Nav, Container } from "reactstrap";
+import { Button, Collapse, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown, NavbarBrand, Navbar, NavItem, Nav } from "reactstrap";
 import StrubloidTooltip from "./StrubloidTooltip";
 
 const SCROLL_THRESHOLD = 500;
@@ -28,58 +28,56 @@ const ScrollTransparentNavbar: React.FC = () => {
     return (
         <>
             {collapseOpen && <div id="bodyClick" onClick={toggleNavOpen} />}
-            <Navbar className={`fixed-top${navbarColor}`} color="white" expand="lg">
-                <Container>
-                    <div className="navbar-translate">
-                        <NavbarBrand href="/" id="navbar-brand">
-                            Rafael Mendes
-                        </NavbarBrand>
-                        <StrubloidTooltip target="navbar-brand">
-                            If you want to know more, keep checking the whole progress of the website on:{" "}
-                            <a href="https://github.com/strubloid/strubloid.com" target="_blank" rel="noopener noreferrer">
-                                click here
-                            </a>
-                        </StrubloidTooltip>
-                        <button onClick={toggleNavOpen} aria-expanded={collapseOpen} className="navbar-toggler">
-                            <span className="navbar-toggler-bar top-bar" />
-                            <span className="navbar-toggler-bar middle-bar" />
-                            <span className="navbar-toggler-bar bottom-bar" />
-                        </button>
-                    </div>
-                    <Collapse isOpen={collapseOpen} navbar>
-                        <Nav className="ml-auto" id="ceva" navbar>
-                            <UncontrolledDropdown nav>
-                                <DropdownToggle
-                                    caret
-                                    color="default"
-                                    data-toggle="dropdown"
-                                    href="#pablo"
-                                    id="navbarDropdownMenuLink"
-                                    nav
-                                    onClick={(e: React.MouseEvent) => e.preventDefault()}
-                                >
-                                    <i aria-hidden={true} className="now-ui-icons design_image" />
-                                    <p>IT Part of me</p>
-                                </DropdownToggle>
-                                <DropdownMenu aria-labelledby="navbarDropdownMenuLink" end>
-                                    <DropdownItem href="/about-me">
-                                        <i className="now-ui-icons business_bulb-63" />
-                                        Who is IT Raf?
-                                    </DropdownItem>
-                                    <DropdownItem href="/contact-me">
-                                        <i className="now-ui-icons location_pin" />
-                                        Contact Me!
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                            <NavItem>
-                                <Button className="nav-link btn-default" color="neutral" href="https://www.linkedin.com/in/strubloid/" target="_blank">
-                                    <p>Linkedin</p>
-                                </Button>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
+            <Navbar className={`fixed-top${navbarColor}`} color="white" expand="lg" container>
+                <div className="navbar-translate">
+                    <NavbarBrand href="/" id="navbar-brand">
+                        Rafael Mendes
+                    </NavbarBrand>
+                    <StrubloidTooltip target="navbar-brand">
+                        If you want to know more, keep checking the whole progress of the website on:{" "}
+                        <a href="https://github.com/strubloid/strubloid.com" target="_blank" rel="noopener noreferrer">
+                            click here
+                        </a>
+                    </StrubloidTooltip>
+                    <button onClick={toggleNavOpen} aria-expanded={collapseOpen} className="navbar-toggler">
+                        <span className="navbar-toggler-bar top-bar" />
+                        <span className="navbar-toggler-bar middle-bar" />
+                        <span className="navbar-toggler-bar bottom-bar" />
+                    </button>
+                </div>
+                <Collapse isOpen={collapseOpen} navbar>
+                    <Nav className="ml-auto" id="ceva" navbar>
+                        <UncontrolledDropdown nav>
+                            <DropdownToggle
+                                caret
+                                color="default"
+                                data-toggle="dropdown"
+                                href="#pablo"
+                                id="navbarDropdownMenuLink"
+                                nav
+                                onClick={(e: React.MouseEvent) => e.preventDefault()}
+                            >
+                                <i aria-hidden={true} className="now-ui-icons design_image" />
+                                <p>IT Part of me</p>
+                            </DropdownToggle>
+                            <DropdownMenu aria-labelledby="navbarDropdownMenuLink" end>
+                                <DropdownItem href="/about-me">
+                                    <i className="now-ui-icons business_bulb-63" />
+                                    Who is IT Raf?
+                                </DropdownItem>
+                                <DropdownItem href="/contact-me">
+                                    <i className="now-ui-icons location_pin" />
+                                    Contact Me!
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        <NavItem>
+                            <Button className="nav-link btn-default" color="neutral" href="https://www.linkedin.com/in/strubloid/" target="_blank">
+                                <p>Linkedin</p>
+                            </Button>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
             </Navbar>
         </>
     );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import type { AppProps } from "next/app";
 
+// Global SCSS (extracted from Now UI Kit Pro v1.3.0)
 import "../public/scss/global/_variables.scss";
 import "../public/scss/global/_buttons.scss";
 import "../public/scss/global/_general_mixins.scss";
 import "semantic-ui-css/semantic.min.css";
-import "../public/css/strubloid.css";
+
+import "../public/scss/global/_now-ui-icons.scss";
+import "../public/scss/global/_typography.scss";
+import "../public/scss/global/_alerts.scss";
+import "../public/scss/global/_forms.scss";
+import "../public/scss/global/_nav-pills-tabs.scss";
+import "../public/scss/global/_dropdowns.scss";
+import "../public/scss/global/_images.scss";
+import "../public/scss/global/_modals.scss";
+import "../public/scss/global/_tables.scss";
+import "../public/scss/global/_info-media.scss";
+import "../public/scss/global/_cards.scss";
+import "../public/scss/global/_pages.scss";
+import "../public/scss/global/_sections.scss";
+import "../public/scss/global/_responsive.scss";
 
 // Component SCSS (compiled at build time by Next.js)
 import "../components/scrapbook/Notes.scss";
@@ -12,6 +27,7 @@ import "../components/scrapbook/Header.scss";
 import "../components/contact/ContactMe.scss";
 import "../components/contact/ContactMeHeader.scss";
 import "../components/shared/BasicHeader.scss";
+import "../components/shared/TransparentNavbar.scss";
 import "../components/shared/Footer.scss";
 import "../components/homepage/AboutMe.scss";
 import "../components/homepage/Github.scss";
