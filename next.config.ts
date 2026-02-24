ffce20825641f8c0728f8b8e900ff683553a6da3
Strubloid <strubloid@gmail.com>
2026-02-24 15:50:58 +0000

    updating the next config

--- next.config.ts
+++ next.config.ts
@@ -6,21 +6,6 @@ const nextConfig: NextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, '')],
     },
-    // Fast Refresh and Development Settings
-    onDemandEntries: {
-        maxInactiveAge: 15 * 1000,
-        pagesBufferLength: 5,
-    },
-    // Webpack configuration for better hot reload
-    webpack: (config, { dev }) => {
-        if (dev) {
-            config.watchOptions = {
-                poll: 2000,
-                aggregateTimeout: 500,
-            };
-        }
-        return config;
-    },
 };
 
 export default nextConfig;
